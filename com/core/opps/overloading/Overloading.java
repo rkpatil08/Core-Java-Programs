40b1785e08f02e4977cf913312588d284ac26422
chetan deore <chetandeore0225@gmail.com>
2025-08-14 21:26:54 +0530

    added overloading program

---
+++ com/core/opps/overloading/Overloading.java
@@ -0,0 +1,18 @@
+package com.core.opps.overloading;
+
+public class Overloading {
+    public static void main(String[] args) {
+        Overloading overloading = new Overloading();
+        overloading.method(10);
+        overloading.method("ABC");
+        overloading.method('A');
+    }
+
+    public void method(int x) {
+        System.out.println("Integer version " + x);
+    }
+
+    public void method(String s) {
+        System.out.println("String version " + s);
+    }
+}
